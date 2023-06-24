c9db86f1e218aa3746dd427e332f4750e21c3002
Roland <75312877+RolandoMagico@users.noreply.github.com>
2023-06-24 17:53:40 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,16 @@
 # Configuration Section
 ################################################################################
 # Freifunk Regensburg Git references
-FFRGB_GIT_URL=https://github.com/ffrgb/site-ffrgb.git
-FFRGB_GIT_BRANCH=v2022.x
+FFRGB_BUILD_GIT_URL=https://github.com/ffrgb/site-ffrgb.git
+FFRGB_BUILD_GIT_BRANCH=v2022.x
+
+# Select gluon targets
+FFRGB_BUILD_GLUON_TARGETS=ramips-mt7621
 
 # Install required build packages
 apt install build-essential gawk unzip libncurses-dev libz-dev libssl-dev wget git subversion
 
 # Clone repository
-git clone ${FFRGB_GIT_URL} -b ${FFRGB_GIT_BRANCH}
+git clone ${FFRGB_BUILD_GIT_URL} -b ${FFRGB_BUILD_GIT_BRANCH}
 cd site-ffrgb
+make GLUON_TARGETS=${FFRGB_BUILD_GLUON_TARGETS}
