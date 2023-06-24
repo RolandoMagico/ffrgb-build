d56e333e22b5d99bcc2af3179f535c909df996f4
Roland <75312877+RolandoMagico@users.noreply.github.com>
2023-06-24 17:47:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+################################################################################
+# Configuration Section
+################################################################################
+# Freifunk Regensburg Git references
+FFRGB_GIT_URL=https://github.com/ffrgb/site-ffrgb.git
+FFRGB_GIT_BRANCH=v2022.x
+
+# Install required build packages
+apt install build-essential gawk unzip libncurses-dev libz-dev libssl-dev wget git subversion
+
+# Clone repository
+git clone ${FFRGB_GIT_URL} -b ${FFRGB_GIT_BRANCH}
+cd site-ffrgb
