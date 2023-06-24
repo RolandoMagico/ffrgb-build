6fec9759502ecf4b9793a58c93cdac3cda008b7d
Roland <75312877+RolandoMagico@users.noreply.github.com>
2023-06-24 18:14:17 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ FFRGB_BUILD_GIT_BRANCH=v2022.x
 
 # Select gluon targets
 FFRGB_BUILD_GLUON_TARGETS=ramips-mt7621
+FFRGB_BUILD_GLUON_DEVICES="d-link-dap-x1860-a1 dlink_covr-x1860-a1"
 
 # Install required build packages
 apt install build-essential gawk unzip libncurses-dev libz-dev libssl-dev wget git subversion
@@ -16,4 +17,4 @@ apt install build-essential gawk unzip libncurses-dev libz-dev libssl-dev wget g
 # Clone repository
 git clone ${FFRGB_BUILD_GIT_URL} -b ${FFRGB_BUILD_GIT_BRANCH}
 cd site-ffrgb
-make GLUON_TARGETS=${FFRGB_BUILD_GLUON_TARGETS}
+make GLUON_TARGETS=${FFRGB_BUILD_GLUON_TARGETS} GLUON_DEVICES=${FFRGB_BUILD_GLUON_DEVICES}
